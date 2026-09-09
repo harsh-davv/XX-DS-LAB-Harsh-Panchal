87030e871bfaded4752b29ea5eee5156b1ad6608
harsh-davv <panchalhharsh@gmail.com>
2026-09-09 13:55:53 +0530

    Add files via upload

---
+++ lab-01/DEMO.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x=10;
+	float y=10.89;
+	char ch='a';
+
+	clrscr();
+	printf("Harsh");
+	getch();
+	/*clrscr();
+	printf("hello..\n");
+	printf("value of x=%d\n",x);
+	printf("value of y=%f\n",y);
+	printf("alue of char=%c",ch);
+	*/
+	getch();
+
+}
